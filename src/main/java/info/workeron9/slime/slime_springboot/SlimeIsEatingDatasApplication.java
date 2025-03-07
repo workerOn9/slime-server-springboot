f5c022ceb1433495279d743b390f47c6944573a1
zhongqihua <zhongqihua@xmiles.cn>
2025-03-07 18:03:40 +0800

    feat: 初始化
    1. 完善gradle配置
    2. 分环境配置文件配置

---
+++ src/main/java/info/workeron9/slime/slime_springboot/SlimeIsEatingDatasApplication.java
@@ -0,0 +1,13 @@
+package info.workeron9.slime.slime_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlimeIsEatingDatasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlimeIsEatingDatasApplication.class, args);
+	}
+
+}
